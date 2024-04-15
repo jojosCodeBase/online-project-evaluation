b228e45dd74d995ad087486c3a366ba1ca91c493
Kunsang Moktan <kunsangmoktan11@gmail.com>
2024-04-16 01:17:16 +0530

    changes in seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
 
         \App\Models\User::factory()->create([
-            'name' => 'Test User',
+            'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make(12345678),
             'role' => 0,
